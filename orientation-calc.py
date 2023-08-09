0eb2f8eea4d90d97754bb8f7cba332491b5489ad
andreasbachmann <132298701+andreasbachmann@users.noreply.github.com>
2023-08-09 09:27:42 +0200

    Update orientation-calc.py

--- orientation-calc.py
+++ orientation-calc.py
@@ -1,8 +1,9 @@
 import math
 
-def angle_from_east(tail, neck):    
+def angle_from_north(tail, neck):    
   delta_easting = neck[0] - tail[0]
   delta_northing = neck[1] - tail[1]
   angle_rad = math.atan2(delta_northing, delta_easting)
   angle_deg = math.degrees(angle_rad)
-  return (angle_deg + 360) % 360
+  angle_from_east = (angle_deg + 360) % 360
+  return (90 - angle_from_east) % 360
